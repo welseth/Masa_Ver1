828b82c743e8ca4f8691f719ebbd32f95b889bf3
wayne.elseth <wayne.elseth@outlook.com>
2016-02-18 11:02:19 -0500

    ANother update.

---
+++ Masa_Ver1/My Project/dbo.Members.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Members] (
+    [Member_refnum] INT            IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (MAX) NULL,
+    [Instructor]    BIT            DEFAULT ((0)) NULL,
+    [Tow Pilot]     BIT            DEFAULT ((0)) NULL,
+    [MASA_Member]   BIT            DEFAULT ((1)) NULL,
+    [Guest_Member]  BIT            DEFAULT ((0)) NULL,
+    PRIMARY KEY CLUSTERED ([Member_refnum] ASC)
+);
+
